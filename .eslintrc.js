a390b363d951a59312650ca1b0c02b32ec00b8cc
Vladimir <vladimirpanteleev@Vladimirs-MacBook-Pro.local>
2017-11-11 13:58:00 +0300

    Add jimp support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "rules": {
+        "indent": ["error", "tab"],
+        "no-tabs": 0
+    }
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
+const jimp = require('jimp');
 
 const app = express();
 
+app.use(fileUpload({
+	limits: { fileSize: 50 * 1024 * 1024 },
+}));
+
+app.post('/upload', async (req, res) => {
+	const lenna = await jimp.read(req.files.file.data);
+
+	lenna.resize(256, 256)
+		.quality(60)
+		.greyscale()
+		.write('lena-small-bw.jpg');
+
+	res.send('File uploaded!');
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+	res.send('Hello World!');
 });
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+	console.log('Example app listening on port 3000!');
 });
